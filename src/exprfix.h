edf65e2d2668d59a390759fc7b0b34322326b7c6
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2020-09-14 20:48:37 +0200

    Add comments to header file

--- src/exprfix.h
+++ src/exprfix.h
@@ -1,4 +1,4 @@
-// Copyright (C) 2019 Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
+// Copyright (C) 2019-20 Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -30,16 +30,60 @@ extern "C" {
 #define PARSE_TOKEN(expr, ptr) strtok(expr, " ")
 #endif
 
+/**
+ * Utility function; determines whether a string describes a binary
+ * operator. This should be implemented by the caller and passed to the
+ * conversion methods.
+ * @param c Token string
+ * @return Integer representation of whether the string is a binary operator
+ */
 typedef int IS_BINARY(char* c);
 
+/**
+ * Utility function; determines whether a string describes a unary
+ * operator. This should be implemented by the caller and passed to the
+ * conversion methods.
+ * @param c Token string
+ * @return Integer representation of whether the string is a unary operator
+ */
 typedef int IS_UNARY(char* c);
 
+/**
+ * Utility function; determines the properties of an operator
+ * @param c Operator
+ * @param precedence Pointer to integer in which to store the operator's precedence level
+ * @param isLeftAssoc Pointer to integer in which to store whether the operator is left associative
+ */
 typedef void OP_PROPERTIES(char* c, int* precedence, int* isLeftAssoc);
 
+/**
+ * Converts an infix expression to a postfix expression
+ * @param expr Expression to convert
+ * @param isBin Function to identify binary operators in the expression
+ * @param isUn Function to identify unary operators in the expression
+ * @param getOpPropos Function to determine operator properties
+ * @return A newly allocated string containing the postfix representation of the given expression
+ */
 char* infixToPostfix(char* expr, IS_BINARY isBin, IS_UNARY isUn, OP_PROPERTIES getOpProps);
 
+/**
+ * Converts a postfix expression to a prefix expression
+ * @param expr Expression to convert
+ * @param isBin Function to identify binary operators in the expression
+ * @param isUn Function to identify unary operators in the expression
+ * @param getOpPropos Function to determine operator properties
+ * @return A newly allocated string containing the prefix representation of the given expression
+ */
 char* postfixToPrefix(char* expr, IS_BINARY isBin, IS_UNARY isUn);
 
+/**
+ * Converts an infix expression to a prefix expression
+ * @param expr Expression to convert
+ * @param isBin Function to identify binary operators in the expression
+ * @param isUn Function to identify unary operators in the expression
+ * @param getOpPropos Function to determine operator properties
+ * @return A newly allocated string containing the prefix representation of the given expression
+ */
 char* infixToPrefix(char* expr, IS_BINARY isBin, IS_UNARY isUn, OP_PROPERTIES getOpProps);
 
 #endif
